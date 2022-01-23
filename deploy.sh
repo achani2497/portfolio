bd56fd4e89272da9576f57eb730e704403239606
Ale Chani <achani@est.frba.utn.edu.ar>
2022-01-23 20:12:25 -0300

    Actualizo el script de deploy y queda funcionando

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:achani2497/portfolio.git main:gh-pages
+git push -f https://github.com/achani2497/portfolio.git master:gh-pages
 
 cd -
