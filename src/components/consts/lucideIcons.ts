a3dac87322a40a88ea457916382cb03319935ebb
Ale Chani <alejandro.chani24@gmail.com>
2026-03-24 12:55:43 -0300

    Mejora de iconos, creo componente Icon y delego la lógica de fallback ahi

---
+++ src/components/consts/lucideIcons.ts
@@ -0,0 +1,10 @@
+import {
+    Cloud,
+    Linkedin,
+    type LucideIcon,
+} from "lucide-react";
+
+export const lucideIcons: Record<string, LucideIcon> = {
+    linkedin: Linkedin,
+    aws: Cloud,
+};
--- src/utils/getIcon.ts
+++ src/utils/getIcon.ts
@@ -1,6 +1,28 @@
+import type { LucideIcon } from "lucide-react";
 import type { SimpleIcon } from "simple-icons";
 import { icons } from "../components/consts/icons";
+import { lucideIcons } from "../components/consts/lucideIcons";
 
-export function getIcon(iconName: string): SimpleIcon | null {
-    return icons[iconName] ?? null;
+type IconResult =
+    | { type: "simple"; icon: SimpleIcon }
+    | { type: "lucide"; icon: LucideIcon }
+    | null;
+
+export function getIconData(iconName: string): IconResult {
+    // intento simple-icons
+    const simpleIcon = icons[iconName];
+
+    if (simpleIcon) {
+        return { type: "simple", icon: simpleIcon };
+    }
+
+
+    // fallback lucide
+    const lucideIcon = lucideIcons[iconName];
+
+    if (lucideIcon) {
+        return { type: "lucide", icon: lucideIcon };
+    }
+
+    return null;
 }
