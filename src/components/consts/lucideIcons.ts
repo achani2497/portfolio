e6034a28600b6f57882a8c03fc23849ec27cbab5
Ale Chani <alejandro.chani24@gmail.com>
2026-03-24 23:17:55 -0300

    Fix de movimiento en menu flotante, modifico iconos

--- src/components/consts/lucideIcons.ts
+++ src/components/consts/lucideIcons.ts
@@ -1,10 +1,18 @@
 import {
+    BriefcaseBusiness,
     Cloud,
+    CodeXml,
+    Home,
     Linkedin,
     type LucideIcon,
 } from "lucide-react";
 
 export const lucideIcons: Record<string, LucideIcon> = {
+    // Missing techs icons on simple-icons
     linkedin: Linkedin,
     aws: Cloud,
+    // Sidebar icons
+    home: Home,
+    experience: BriefcaseBusiness,
+    technologies: CodeXml
 };
